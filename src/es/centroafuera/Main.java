6885bdb7356df980ad30a093965e4ef400936821
Alba Martín <alba@alba.com>
2021-11-23 10:25:40 +0100

    Cuarto commit MAIN

--- src/es/centroafuera/Main.java
+++ src/es/centroafuera/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola");
-        System.out.println("Qué tal estas?");
+        System.out.println("¿Qué tal estás?");
         System.out.println("Estoy bien");
     }
 }
