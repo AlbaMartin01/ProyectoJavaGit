e6d8c6895bd7471abbe31a403e30f6abf879f0ed
AlbaMartin01 <94897368+AlbaMartin01@users.noreply.github.com>
2021-11-23 10:24:20 +0100

    Update Main.java
    Estoy bien añadido

--- src/es/centroafuera/Main.java
+++ src/es/centroafuera/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Qué tal estas?");
+        System.out.println("Estoy bien");
     }
 }
